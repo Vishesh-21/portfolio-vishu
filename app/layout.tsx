4d76355f1f50acc353ffbe9dd012dbe98757eac1
Vishu Verma <visheshbaund@gmail.com>
2026-01-14 13:16:18 +0530

    Initialize project setup with basic dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+// @ts-ignore
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="max-w-5xl mx-auto">{children}</main>
       </body>
     </html>
   );
